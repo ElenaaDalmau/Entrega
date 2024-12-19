e8ac6f50ec784b24808ff3b15a560d1cfd1a234e
ElenaaDalmau <elenadalmau6@gmail.com>
2024-12-19 17:26:37 +0100

    main

--- php/inicioESPE.php
+++ php/inicioESPE.php
@@ -20,7 +20,7 @@
 <html lang="es">
 
     <head>
-        
+         
         <meta charset="utf-8">
         
         <title> Login </title>    
@@ -31,7 +31,7 @@
         <link href="https://fonts.googleapis.com/css?family=Overpass&display=swap" rel="stylesheet">
         
         <!-- Link hacia el archivo de estilos css -->
-        <link rel="stylesheet" href="../css/estilo.css">
+        <link rel="stylesheet" href="../css/InicioEspe.css">
 
         <!-- Link favicon -->
         <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
@@ -52,27 +52,7 @@
         <nav>
             <ul>
                 <?php
-                if ($_SESSION['Tipo'] == "cliente") { // Si es Admin, mostrar opciones adicionales
                     echo "<li><a href='../index.php'><i class='fa fa-home'></i> <span data-translate='inicio'>Inicio</span></a></li>";
-                    echo "<li><a href='ComoTrabajamos.php'><i class='fa fa-briefcase'></i> <span data-translate='como_trabajar'>¿Quiénes somos?</span></a></li>";
-                    echo "<li><a href='Contacto.php'><i class='fa fa-phone-square'></i> <span data-translate='contacto'>Puesta en contacto</span></a></li>";
-                    echo "<li><a href='ListadoEspe.php'><i class='fa fa-address-book'></i> <span data-translate='especialistas'>Especialistas</span></a></li>";
-                    echo "<li><a href='Calendario.php'><i class='fa fa-calendar'></i> <span data-translate='calendario'>Calendario</span></a></li>";
-                    echo '<br>';
-                }
-
-                if ($_SESSION['Tipo'] == "admin") { // Si es Admin, mostrar opciones adicionales
-                    echo "<li><a href='../index.php'><i class='fa fa-home'></i> <span data-translate='inicio'>Inicio</span></a></li>";
-                    echo "<li><a href='ComoTrabajamos.php'><i class='fa fa-briefcase'></i> <span data-translate='como_trabajar'>¿Quiénes somos?</span></a></li>";
-                    echo "<li><a href='FuncionesAdmin.php'><i class='fa fa-address-book'></i><span data-translate='ADMIN'>Admin</span></a></li>";
-                    echo '<br>';
-                }
-                if ($_SESSION['Tipo'] == "espe") { // Si es Especialista, mostrar opciones adicionales
-                    echo "<li><a href='../index.php'><i class='fa fa-home'></i> <span data-translate='inicio'>Inicio</span></a></li>";
-                    echo "<li><a href='ComoTrabajamos.php'><i class='fa fa-briefcase'></i> <span data-translate='como_trabajar'>¿Quiénes somos?</span></a></li>";
-                    echo "<li><a href='FuncionesEspe.php'><i class='fa fa-address-book'></i><span data-translate='espe'>espe</span></a></li>";
-                    echo '<br>';
-                }
                 ?>
                 <li>               
                     <div class="lenguage-selector">
@@ -98,56 +78,74 @@
     <?php
 
 // Verificar conexión
-if (!$conn) {
-    die("Error en la conexión a la base de datos: " . mysqli_connect_error());
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['IniciarSesion'])) {
-    $DNI_Especialista = mysqli_real_escape_string($conn, $_POST['DNI_Especialista']);
-    $Contrasena_Especialista = mysqli_real_escape_string($conn, $_POST['Contrasena_Especialista']);
-
-    $sql = "SELECT * FROM ESPECIALISTAS WHERE DNI_Especialista = ?";
-    $stmt = mysqli_prepare($conn, $sql);
-
-    // Verificar si la preparación fue exitosa
-    if (!$stmt) {
-        die("Error en la preparación de la consulta: " . mysqli_error($conn));
+    if (!$conn) {
+        die("Error en la conexión a la base de datos: " . mysqli_connect_error());
     }
 
-    mysqli_stmt_bind_param($stmt, 's', $DNI_Especialista);
-    mysqli_stmt_execute($stmt);
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['IniciarSesion'])) {
+        $DNI_Especialista = mysqli_real_escape_string($conn, $_POST['DNI_Especialista']);
+        $Contrasena_Especialista = mysqli_real_escape_string($conn, $_POST['Contrasena_Especialista']);
 
-    $result = mysqli_stmt_get_result($stmt);
+        $sql = "SELECT * FROM ESPECIALISTAS WHERE DNI_Especialista = ?";
+        $stmt = mysqli_prepare($conn, $sql);
 
-    // Verificar si el resultado es válido
-    if ($result && mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_assoc($result);
-
-        // Validar contraseña
-        if ($Contrasena_Especialista === $row['Contrasena_Especialista']) {
-            $_SESSION['Tipo'] = "espe";
-            $_SESSION['DNI_Especialista'] = $row['DNI_Especialista'];
-            header("Location: ComoTrabajamos.php");
-            exit;
-        } else {
-            echo "<script>alert('Contraseña incorrecta');</script>";
+        // Verificar si la preparación fue exitosa
+        if (!$stmt) {
+            die("Error en la preparación de la consulta: " . mysqli_error($conn));
         }
-    } else {
-        echo "<script>alert('Usuario no encontrado');</script>";
-    }
-}
-?>
 
-        <form action="" method="post">
-            <h1>Iniciar Sesión</h1>
-            <input type="text" name="DNI_Especialista" required placeholder="Correo">
-            <input type="password" name="Contrasena_Especialista" required placeholder="Contraseña">
-            <button type="submit" name="IniciarSesion">Iniciar Sesión</button>
-        </form>
-    </div>
-</div>
+        mysqli_stmt_bind_param($stmt, 's', $DNI_Especialista);
+        mysqli_stmt_execute($stmt);
+
+        $result = mysqli_stmt_get_result($stmt);
+
+        // Verificar si el resultado es válido
+        if ($result && mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+
+            // Validar contraseña
+            if ($Contrasena_Especialista === $row['Contrasena_Especialista']) {
+                $_SESSION['Tipo'] = "espe";
+                $_SESSION['DNI_Especialista'] = $row['DNI_Especialista'];
+                header("Location: ComoTrabajamos.php");
+                exit;
+            } else {
+                echo "<script>alert('Contraseña incorrecta');</script>";
+            }
+        } else {
+            echo "<script>alert('Usuario no encontrado');</script>";
+        }
+    }
+    else{
+    ?>
+
+    <div class="centrar_info">
+        <div id="recuperar_global">
+            <div id="recuperar_central">
+                <div id="login">
+                    <div class="recuperar_titulo">Iniciar sesión</div>
+                        <form action="" method="post">
+                            <input type="text" name="DNI_Especialista" required placeholder="Correo">
+                            <input type="password" name="Contrasena_Especialista" required placeholder="Contraseña">
+                            <button type="submit" name="IniciarSesion">Iniciar Sesión</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- recuperar_global -->
+    </div> <!-- centrar_info -->
+
+        <?php
+            }
+        ?>
+
+<!-- PIE DE PAGINA -->
+    <footer>
+        Todos los derechos reservados | Coaching SL Copyright © 2024
+    </footer>
 
 <!-- Link a JavaScript -->
-<script src="../JS/traducciones.js"></script>
+    <script src="../JS/traducciones.js"></script>
+
 </body>
 </html>
